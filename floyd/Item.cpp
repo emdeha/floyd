3fdadce8fd22d44ac0c50aad9f89b176866d51a9
cpi-cecko <cpi.cecko@gmail.com>
2014-06-30 11:07:58 +0300

    implements serialization for Item

--- floyd/Item.cpp
+++ floyd/Item.cpp
@@ -1,5 +1,8 @@
 #include "Item.h"
 
+#include <fstream>
+#include <iostream>
+
 
 Item::Item()
 	: name(""), defense(0), damage(0), health(0), attribute(ATTRIB_NONE), position() {}
@@ -44,7 +47,35 @@ bool Item::IsValid() const
 
 void Item::Serialize(std::ofstream &saveStream) const
 {
+	if (saveStream.is_open())
+	{
+		saveStream << name;
+		saveStream << defense;
+		saveStream << damage;
+		saveStream << health;
+		saveStream << (int)attribute;
+		position.Serialize(saveStream);
+	}
+	else
+	{
+		std::cerr << "Error: Cannot serialize Item\n";
+	}
 }
 void Item::Deserialize(std::ifstream &loadStream)
 {
+	if (loadStream.is_open())
+	{
+		loadStream >> name;
+		loadStream >> defense;
+		loadStream >> damage;
+		loadStream >> health;
+		int itemAttrib = -999;
+		loadStream >> itemAttrib;
+		attribute = (ItemAttribute)itemAttrib;
+		position.Deserialize(loadStream);
+	}
+	else
+	{
+		std::cerr << "Error: Cannot deserialize Item\n";
+	}
 }
