7ee44d4fb8f1d0d80cbc4bc52d6c1fa1d8526bb8
cpi-cecko <cpi.cecko@gmail.com>
2014-07-04 20:20:52 +0300

    implements components

--- floyd/Component.h
+++ floyd/Component.h
@@ -2,6 +2,13 @@
 #define FLOYD_COMPONENT_H
 
 
+#include <time.h>
+#include <vector>
+#include <string>
+
+#include "Utils.h"
+
+
 enum ComponentType
 {
 	CTYPE_STAT,
@@ -16,12 +23,107 @@ enum ComponentType
 	CTYPE_INVALID = -1
 };
 
-struct IComponent
+class IComponent
 {
+private:
+	virtual void OnUpdate() = 0;
+
+public:
 	ComponentType cType;
+
+	void Update();
 
 	IComponent(ComponentType newCType);
 	virtual ~IComponent();
+};
+
+class StatComponent : public IComponent
+{
+public:
+	int health;
+	int defense;
+	int damage;
+	int maxHealth;
+
+	StatComponent(int newHealth, int newDefense, int newDamage, int newMaxHealth);
+};
+
+class ParticleEmitterComponent : public IComponent
+{
+public:
+	time_t particleEmitInterval_s;
+	time_t lastTimeOfEmission_s;
+	int particlesPerEmission;
+
+	ParticleEmitterComponent(time_t newParticleEmitInterval_s, time_t newLastTimeOfEmission_s,
+							 int newParticlesPerEmission);
+
+private:
+	void OnUpdate();
+};
+
+class MovableComponent : public IComponent
+{
+public:
+	Position position;
+	Position prevPosition;
+	Position direction;
+	char prevTile;
+
+	MovableComponent(const Position &newPosition, const Position &newPrevPosition, const Position &newDirection,
+					 char newPrevTile);
+
+private:
+	void OnUpdate();
+};
+
+class Entity;
+
+class OwnableComponent : public IComponent
+{
+public:
+	Entity *owner;
+
+	OwnableComponent(Entity *newOwner);
+	~OwnableComponent();
+};
+
+class ControllableComponent : public IComponent
+{
+public:
+	ControllableComponent();
+
+private:
+	void OnUpdate();
+};
+
+class AIComponent : public IComponent
+{
+public:
+	AIComponent();
+
+private:
+	void OnUpdate();
+};
+
+class Skill;
+
+class InventoryComponent : public IComponent
+{
+public:
+	std::vector<Skill*> skills;
+	std::vector<std::string> ownedItemNames;
+
+	InventoryComponent(); // We should add them manually.
+};
+
+class CollidableComponent : public IComponent
+{
+public:
+	CollidableComponent();
+
+private:
+	void OnUpdate();
 };
 
 
